9769bb823d45e76e3c03a6a08ab79fe00a9d9a5d
minji-yoo-accounting <160660700+minji-yoo-accounting@users.noreply.github.com>
2024-07-13 15:25:15 -0400

    Create top_k_ensemble_gpt.sh

---
+++ scripts/top_k_ensemble_gpt.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# prompt strategy = top-k
+# sampling strategy = self-random ->  by setting NUM_ENSEMBLE to decide the number of samples we want to draw from a given question
+# aggregator = consistency / avg-conf / pair-rank (all of them will be automatically computed in the same script)
+PROMPT_TYPE="top_k"
+SAMPLING_TYPE="self_random" 
+NUM_ENSEMBLE=5 
+CONFIDENCE_TYPE="${PROMPT_TYPE}_${SAMPLING_TYPE}_${NUM_ENSEMBLE}"
+
+
+
+# TODO uncomment following lines to run on different settings
+#############################################################
+
+DATASET_NAME="Financial_PhraseBank"
+MODEL_NAME="chatgpt"
+TASK_TYPE="multi_choice_qa"
+DATASET_PATH="dataset/MMLU/professional_law_test.csv"
+USE_COT=false # use cot or not
+TEMPERATURE=0.7
+TOP_K=4
+
+
+#############################################################
+# set time stamp to differentiate the output file
+TIME_STAMPE=$(date "+%m-%d-%H-%M")
+
+OUTPUT_DIR="final_output/$CONFIDENCE_TYPE/$MODEL_NAME/$DATASET_NAME"
+RESULT_FILE="$OUTPUT_DIR/${DATASET_NAME}_${MODEL_NAME}_${TIME_STAMPE}.json"
+USE_COT_FLAG=""
+
+if [ "$USE_COT" = true ] ; then
+    USE_COT_FLAG="--use_cot"
+fi
+
+python query_top_k.py \
+   --dataset_name  $DATASET_NAME \
+   --data_path $DATASET_PATH \
+   --output_file  $RESULT_FILE \
+   --model_name  $MODEL_NAME \
+   --task_type  $TASK_TYPE  \
+   --prompt_type $PROMPT_TYPE \
+   --num_K $TOP_K \
+   --sampling_type $SAMPLING_TYPE \
+   --num_ensemble $NUM_ENSEMBLE \
+   --temperature_for_ensemble $TEMPERATURE \
+   $USE_COT_FLAG
+
+
+# uncomment following lines to run test and visualization
+python extract_answers.py \
+   --input_file $RESULT_FILE \
+   --model_name  $MODEL_NAME \
+   --dataset_name  $DATASET_NAME \
+   --task_type  $TASK_TYPE   \
+   --prompt_type $PROMPT_TYPE \
+   --num_K $TOP_K \
+   --sampling_type $SAMPLING_TYPE \
+   --num_ensemble $NUM_ENSEMBLE \
+    $USE_COT_FLAG
+
+RESULT_FILE_PROCESSED=$(echo $RESULT_FILE | sed 's/\.json$/_processed.json/')
+
+python vis_aggregated_conf_top_k.py \
+    --input_file $RESULT_FILE_PROCESSED \
+    --model_name  $MODEL_NAME \
+    --dataset_name  $DATASET_NAME \
+    --task_type  $TASK_TYPE   \
+    --prompt_type $PROMPT_TYPE  \
+    --num_K $TOP_K \
+    --sampling_type $SAMPLING_TYPE \
+    --num_ensemble $NUM_ENSEMBLE \
+    $USE_COT_FLAG    
