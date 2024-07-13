52400349e2743291a4b1c338de6a620609ff9cf8
minji-yoo-accounting <160660700+minji-yoo-accounting@users.noreply.github.com>
2024-07-13 15:11:44 -0400

    Create cot.sh

---
+++ scripts/cot.sh
@@ -0,0 +1,66 @@
+
+PROMPT_TYPE="cot"
+SAMPLING_TYPE="self_random" 
+NUM_ENSEMBLE=1
+CONFIDENCE_TYPE="${PROMPT_TYPE}_${SAMPLING_TYPE}_${NUM_ENSEMBLE}"
+
+
+
+# TODO uncomment following lines to run on different settings
+#############################################################
+
+DATASET_NAME="Professional_Law"
+MODEL_NAME="chatgpt"
+TASK_TYPE="multi_choice_qa"
+DATASET_PATH="/content/drive/MyDrive/llm_trust/bank.csv"
+USE_COT=true # use cot or not
+TEMPERATURE=0.0
+
+#############################################################
+# set time stamp to differentiate the output file
+TIME_STAMPE=$(date "+%m-%d-%H-%M")
+
+OUTPUT_DIR="final_output/$CONFIDENCE_TYPE/$MODEL_NAME/$DATASET_NAME"
+RESULT_FILE="$OUTPUT_DIR/${DATASET_NAME}_${MODEL_NAME}_${TIME_STAMPE}.json"
+USE_COT_FLAG=""
+
+if [ "$USE_COT" = true ] ; then
+    USE_COT_FLAG="--use_cot"
+fi
+
+
+python query_top_k.py \
+   --dataset_name  $DATASET_NAME \
+   --data_path $DATASET_PATH \
+   --output_file  $RESULT_FILE \
+   --model_name  $MODEL_NAME \
+   --task_type  $TASK_TYPE  \
+   --prompt_type $PROMPT_TYPE \
+   --sampling_type $SAMPLING_TYPE \
+   --num_ensemble $NUM_ENSEMBLE \
+   --temperature_for_ensemble $TEMPERATURE \
+   $USE_COT_FLAG
+
+
+# uncomment following lines to run test and visualization
+python extract_answers.py \
+   --input_file $RESULT_FILE \
+   --model_name  $MODEL_NAME \
+   --dataset_name  $DATASET_NAME \
+   --task_type  $TASK_TYPE   \
+   --prompt_type $PROMPT_TYPE \
+   --sampling_type $SAMPLING_TYPE \
+   --num_ensemble $NUM_ENSEMBLE \
+    $USE_COT_FLAG
+
+RESULT_FILE_PROCESSED=$(echo $RESULT_FILE | sed 's/\.json$/_processed.json/')
+
+python vis_aggregated_conf.py \
+    --input_file $RESULT_FILE_PROCESSED \
+    --model_name  $MODEL_NAME \
+    --dataset_name  $DATASET_NAME \
+    --task_type  $TASK_TYPE   \
+    --prompt_type $PROMPT_TYPE  \
+    --sampling_type $SAMPLING_TYPE \
+    --num_ensemble $NUM_ENSEMBLE \
+    $USE_COT_FLAG    
