91d190f494792a1428639ad3d5962f48671771c5
Minji Yoo <160660700+minji-yoo-accounting@users.noreply.github.com>
2025-07-11 11:05:01 -0400

    o4-mini

--- scripts/top_k_ensemble_gpt.sh
+++ scripts/top_k_ensemble_gpt.sh
@@ -18,21 +18,21 @@ CONFIDENCE_TYPE="${PROMPT_TYPE}_${SAMPLING_TYPE}_${NUM_ENSEMBLE}"
 # TODO uncomment following lines to run on different settings
 #############################################################
 
-# DATASET_NAME="Financial_PhraseBank"
-# MODEL_NAME="chatgpt"
-# TASK_TYPE="multi_choice_qa"
-# DATASET_PATH="C:/Users/minjiyoo/Desktop/llm-uncertainty/bank.csv"
-# USE_COT=false # use cot or not
-# TEMPERATURE=0.7
-# TOP_K=3
-
-DATASET_NAME="ReutersNews"
-MODEL_NAME="chatgpt"
+DATASET_NAME="Financial_PhraseBank"
+MODEL_NAME="o4-mini"
 TASK_TYPE="multi_choice_qa"
-DATASET_PATH="/content/llm-trust-issue/articles_subset7.csv"
+DATASET_PATH="/Users/minjiyoo/Documents/GitHub/llm-trust-issue/bank.csv"
 USE_COT=false # use cot or not
 TEMPERATURE=0.7
 TOP_K=3
+
+# DATASET_NAME="ReutersNews"
+# MODEL_NAME="chatgpt"
+# TASK_TYPE="multi_choice_qa"
+# DATASET_PATH="/content/llm-trust-issue/articles_subset7.csv"
+# USE_COT=false # use cot or not
+# TEMPERATURE=0.7
+# TOP_K=3
 
 #############################################################
 # set time stamp to differentiate the output file
