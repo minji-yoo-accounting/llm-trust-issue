9a1481a2b54f3e76accd4e2fb15beaae86ef2f4c
Minji Yoo <160660700+minji-yoo-accounting@users.noreply.github.com>
2025-07-11 00:33:54 -0400

    o4-mini setting 1

--- scripts/cot_gpt.sh
+++ scripts/cot_gpt.sh
@@ -14,22 +14,22 @@ CONFIDENCE_TYPE="${PROMPT_TYPE}_${SAMPLING_TYPE}_${NUM_ENSEMBLE}"
 # TODO uncomment following lines to run on different settings
 #############################################################
 
-# DATASET_NAME="Financial_PhraseBank"
-# MODEL_NAME="chatgpt"
-# TASK_TYPE="multi_choice_qa"
-# DATASET_PATH="C:/Users/minjiyoo/Desktop/llm-uncertainty/bank.csv"
-# USE_COT=true # use cot or not
-# TEMPERATURE=0.0
-# TOP_K=1
-
-
-DATASET_NAME="ReutersNews"
-MODEL_NAME="chatgpt"
+DATASET_NAME="Financial_PhraseBank"
+MODEL_NAME="o4-mini"
 TASK_TYPE="multi_choice_qa"
-DATASET_PATH="C:/Users/minjiyoo/Desktop/llm-uncertainty/articles_subset7.csv"
+DATASET_PATH="/Users/minjiyoo/Documents/GitHub/llm-trust-issue/bank.csv"
 USE_COT=true # use cot or not
 TEMPERATURE=0.0
 TOP_K=1
+
+
+# DATASET_NAME="ReutersNews"
+# MODEL_NAME="chatgpt"
+# TASK_TYPE="multi_choice_qa"
+# DATASET_PATH="C:/Users/minjiyoo/Desktop/llm-uncertainty/articles_subset7.csv"
+# USE_COT=true # use cot or not
+# TEMPERATURE=0.0
+# TOP_K=1
 
 
 #############################################################
